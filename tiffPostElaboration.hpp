ce6c8afa64696b1d028c615060f1ba45900c4090
andre.lotta86 <andre.lotta86@f35a090e-7ae0-11de-9755-51fe06bdd687>
2009-08-28 23:14:35 +0000

    tutto forse

---
+++ tiffPostElaboration.hpp
@@ -0,0 +1,7 @@
+bool writeImage(unsigned char* a,char *dest, int w, int h);
+
+void octaveVectorPort(unsigned char *data,int length);
+
+void octaveMatrixPort(unsigned char *data,int width,int length);
+
+void matlabMatrixPort(unsigned char *data,int width,int length);
