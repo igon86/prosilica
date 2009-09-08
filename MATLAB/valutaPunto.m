4198fb0615626b9e5437fa70fe9799913b9db3f2
andre.lotta86 <andre.lotta86@f35a090e-7ae0-11de-9755-51fe06bdd687>
2009-09-08 18:52:16 +0000

    prova configure

--- MATLAB/valutaPunto.m
+++ MATLAB/valutaPunto.m
@@ -1,4 +1,4 @@
 function z = valutaPunto(A,x_0,y_0,sigma_x,sigma_y,a,b,c,x,y)
-    %this function just evaluate the 2D gaussian in (x,y) 
+    %this function just evaluate the 2D gaussian in (x,y)
     z= (A*exp(-((x-x_0)^2/sigma_x^2 + (y-y_0)^2/sigma_y^2))) +a*x +b*y + c;
 end
