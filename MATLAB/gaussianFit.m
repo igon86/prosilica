fec1f8c9942d7cb19e3722b1202f385da1da49c2
andre.lotta86 <andre.lotta86@f35a090e-7ae0-11de-9755-51fe06bdd687>
2009-09-11 06:03:21 +0000

    SBARABLEU FUNZIONA!!

--- MATLAB/gaussianFit.m
+++ MATLAB/gaussianFit.m
@@ -9,8 +9,9 @@ function [A,x_0,y_0,sigma_x,sigma_y,a,b,c] = gaussianFit(image,max,min,centro_x,
     title('toBeFitted');
     
     %scelgo quanto iterare (ancora non funziona abbastanza bene da poter usare i residui come criterio di areesto)
-    iterazioni = 10;
+    iterazioni = 1;
     R = zeros(iterazioni,1);
+    data = image(:);
     
     %parameter inizialization
     A = max;
@@ -29,6 +30,10 @@ function [A,x_0,y_0,sigma_x,sigma_y,a,b,c] = gaussianFit(image,max,min,centro_x,
     differenze = zeros(m,1);
     immagine = zeros(m,1);
     M = zeros(m,6);
+    %pixel TEST
+    fp = fopen('test.mat','w');
+    fprintf(fp,'%d\n%d\n%d\n%d\n',image(1,1),image(1,2),image(1,3),image(1,4));
+    
     
     %predition plot
     for i=1:m
@@ -55,8 +60,8 @@ function [A,x_0,y_0,sigma_x,sigma_y,a,b,c] = gaussianFit(image,max,min,centro_x,
 %             if mask(i) == 1
                    differenze(i) = img(i) - test;
 %             end
-            if i <=200
-                fprintf(fip,'%8f\n',img(i));
+            if i <=500
+                fprintf(fip,'%d\n',data(i));
             end
             
             M(i,1) = 1/exp((x - x_0)^2/sigma_x^2 + (y - y_0)^2/sigma_y^2);
@@ -88,16 +93,16 @@ function [A,x_0,y_0,sigma_x,sigma_y,a,b,c] = gaussianFit(image,max,min,centro_x,
         %calcolo la matrice di iterazione a e b
         matrix = M'*M;
         for k=1:8
-        fprintf(1,'%8f\t %8f\t %8f\t %8f\t %8f\t %8f\t %8f\t %8f\n',matrix(k,1),matrix(k,2),matrix(k,3),matrix(k,4),matrix(k,5),matrix(k,6),matrix(k,7),matrix(k,8));
+        %fprintf(1,'%8f\t %8f\t %8f\t %8f\t %8f\t %8f\t %8f\t %8f\n',matrix(k,1),matrix(k,2),matrix(k,3),matrix(k,4),matrix(k,5),matrix(k,6),matrix(k,7),matrix(k,8));
         end
         vector = M'*differenze;
-        fprintf(1,'\nVETTORE\n%8f\t %8f\t %8f\t %8f\t %8f\t %8f\t %8f\t %8f\n',vector(1),vector(2),vector(3),vector(4),vector(5),vector(6),vector(7),vector(8));
+        %fprintf(1,'\nVETTORE\n%8f\t %8f\t %8f\t %8f\t %8f\t %8f\t %8f\t %8f\n',vector(1),vector(2),vector(3),vector(4),vector(5),vector(6),vector(7),vector(8));
 
         %guardo il livello di schifezza
         R(j) = differenze'*differenze;
 
         %risolvo il sistema lineare per avere delta
-        delta = matrix\vector;
+        delta = matrix\vector
         
 %         fprintf(1,'%8f\t %8f\t %8f\t %8f\t %8f\t %8f\t %8f\t %8f\n',delta(1),delta(2),delta(3),delta(4),delta(5),delta(6),delta(7),delta(8));
 
