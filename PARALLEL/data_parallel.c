49d0c5847aa59ba686043bbe904bd1fcdba5528a
andre.lotta86 <andre.lotta86@f35a090e-7ae0-11de-9755-51fe06bdd687>
2010-09-14 15:45:38 +0000

    e anco il data parallel

--- PARALLEL/data_parallel.c
+++ PARALLEL/data_parallel.c
@@ -13,6 +13,9 @@ int main(int argc, char *argv[])
     /* pixels per worker */
     int ppw = 0;
 	
+	/* number of workers */
+	int num_worker;
+	
 	/* pixels of the entire image */
 	int dim;
 	
@@ -30,27 +33,37 @@ int main(int argc, char *argv[])
 	
     /* image representing Gaussian fit */
     unsigned char *image = NULL;
+#ifdef PADDED
+	unsigned char *padded = NULL;
+#endif	
 	
     /* local partition of the image*/
     unsigned char *partition;
+	
+	/* size of reduce buffer */
+	int buffer_size = DIM_FIT * (DIM_FIT + 1);
 	
     /* indexes */
     int i = 0;
 	
 	/* buffer for fit procedure */
-    double *data = (double *) malloc(sizeof(double) * DIM_FIT * (DIM_FIT + 1));
+    double *data = (double *) malloc( sizeof(double) * buffer_size );
     gsl_matrix_view matrice = gsl_matrix_view_array(data, DIM_FIT, DIM_FIT);
     gsl_vector_view vettore = gsl_vector_view_array(data + (DIM_FIT * DIM_FIT), DIM_FIT);
 	
     /* output buffer of reduce */
-    double *ret = (double *) malloc(sizeof(double) * DIM_FIT * (DIM_FIT + 1));
+    double *ret = (double *) malloc( sizeof(double) * buffer_size );
     gsl_matrix_view r_matrice = gsl_matrix_view_array(ret, DIM_FIT, DIM_FIT);
     gsl_vector_view r_vettore = gsl_vector_view_array(ret + (DIM_FIT * DIM_FIT), DIM_FIT);
+	
+	/*********************************************************************
+	 INIT
+	 *********************************************************************/	
 	
     /* Initialize of MPI */
     MPI_Init(&argc, &argv);
 	
-	/* check the input parameters */
+	/* Check the input parameters */
     if (argc != 3) {
 		fprintf(stderr, "Invalid number of parameters: %d\n",argc);
 		MPI_Abort(MPI_COMM_WORLD,MPI_ERR_ARG);
@@ -62,9 +75,8 @@ int main(int argc, char *argv[])
     /* Total number of processes */
     MPI_Comm_size(MPI_COMM_WORLD, &p);
 	
-	/*********************************************************************
-	 INIT
-	 *********************************************************************/
+	/* Number of workers */ 
+	num_worker = p - PS;
 	
     if (my_rank == EMITTER) {
 #if DEBUG
@@ -76,7 +88,7 @@ int main(int argc, char *argv[])
 		width = atoi(argv[1]);
 		height = atoi(argv[2]);
 		/* y dimension of the image is adjusted (if needed) */
-		while (height % p != 0) height++;
+		while (height % num_worker != 0) height++;
 		/* image is created */
 		image = createImage(width, height);
 		
@@ -84,7 +96,7 @@ int main(int argc, char *argv[])
 		initialization(image, width, height,fit);
 		
 #if DEBUG
-		printf("Emitter sopravvissuto alla init\n");
+		printf("Emitter survived init\n");
 #endif	
 		
     } 
@@ -100,9 +112,10 @@ int main(int argc, char *argv[])
 #endif
 	/* dimension of the local partition are determined and relative buffers
 	 are initialized */  
+	
 	dim = width * height;
 	dimx = width;
-	dimy = height / p;
+	dimy = height / num_worker;
     ppw = dimx * dimy;
     partition = (unsigned char *) malloc(sizeof(unsigned char) * ppw);
     initBuffers(ppw);
@@ -110,6 +123,20 @@ int main(int argc, char *argv[])
     /* if I am the emitter I take the time */
     if (my_rank == EMITTER)
 		gettimeofday(&tv1, NULL);
+		
+#ifdef PADDED
+	if (my_rank == EMITTER){
+		padded =(unsigned char*) malloc(sizeof(unsigned char)*ppw*p);
+		for (i=0; i < dim; i++){
+			padded[i + ppw] = image[i];
+		}
+		for(i=0;i<DIM_FIT * (DIM_FIT + 1);i++){
+			ret[i] = 0;
+		}		
+		free(image);
+		image = padded;
+	}	
+#endif	
 	
     /*********************************************************************
 	 LOOP on ELEMENTS
@@ -119,10 +146,11 @@ int main(int argc, char *argv[])
 		
 		/* the emitter executes the scatter */
 		MPI_Scatter(image, ppw, MPI_UNSIGNED_CHAR, partition, ppw, MPI_UNSIGNED_CHAR, EMITTER, MPI_COMM_WORLD);
-		/* execute the procedure over my partition */
-		procedure(partition, dimx, dimy , fit, matrice, vettore,dimy*my_rank);
+		/* execute the procedure over my partition */		
+		if(my_rank >= PS)
+			procedure(partition, dimx, dimy , fit, matrice, vettore, dimy*(my_rank-PS));
 		
-#ifdef PADDED
+#ifdef ALL
 		/* execute the reduce of matrix and vector */
 		MPI_Allreduce(data, ret, DIM_FIT * (DIM_FIT + 1), MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
 		
--- PARALLEL/parallel.h
+++ PARALLEL/parallel.h
@@ -20,7 +20,11 @@
 #endif
 
 #ifdef DATA_PARALLEL
+#ifdef PADDED
+#define PS 1
+#else
 #define PS 0
+#endif
 #endif
 
 /* define MPI tags of the messages */
