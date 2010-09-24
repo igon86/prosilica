9b08c2d0a02dce9c817ee4ec0ef6fe150edee614
andre.lotta86 <andre.lotta86@f35a090e-7ae0-11de-9755-51fe06bdd687>
2010-09-24 15:51:24 +0000

    SCRIPTONE

--- PARALLEL/SCRIPT/generatore.sh
+++ PARALLEL/SCRIPT/generatore.sh
@@ -1,26 +1,69 @@
 #!/bin/bash
 
+#grado di parallelismo della macchina
 np=16
 x=10
 y=10
-i=1
+#grado di parallelismo minimo
+#indici
+k=1
 
-FLAGS="-o3 -DON_DEMAND -DFARM -DMODULE"
+FILE_FLAG=./FLAG_FARM
+FILE_DIM=./DIMENSIONI
 
-echo "mpicc -D_X86 -D_OSX -mmacosx-version-min=10.4 -Wall -pedantic $FLAGS -I/opt/local/var/macports/software/gsl/1.12_0/opt/local/include -lgsl -lgslcblas -lm -ltiff ./farm.c ./fit.c ./image.c ./macro.c -L/opt/local/var/macports/software/gsl/1.12_0/opt/local/lib -o farm" > test.sh
+num_flag=$(cat $FILE_FLAG | wc -l )
+num_dim=$(cat $FILE_DIM | wc -l )
+echo "numero di diversi flag: $num_flag"
+echo "numero di diverse dimensioni: $num_dim"
 
-for j in $FLAGS;
+#apro il file
+echo "echo INIZIO" > test.sh
+#diversi flag di compilazione
+while [ $k -le $num_flag ]
 do
-TAGNAME="$TAGNAME$j"
-done
+	#mi piglio la riga di parametri
+	FLAGS=$(head -n $k $FILE_FLAG | tail -n 1)
+	#e il grado di parallelismo minimo scritto nella riga sotto
+	k=$[$k+1]
+	MIN_P=$(head -n $k $FILE_FLAG | tail -n 1)
+	# inizializzo il nome del file
+	FLAG_TAGNAME=""
+	for j in $FLAGS;
+	do
+		#compilo il nome per il file in output (i flag sono tutti attaccati)
+		FLAG_TAGNAME="$FLAG_TAGNAME$j"
+	done
+	echo $FLAG_TAGNAME
+	
+	#COMPILO
+	echo "mpicc $FLAGS -Wall -pedantic  -I/opt/local/var/macports/software/gsl/1.12_0/opt/local/include -lgsl -lgslcblas -lm -ltiff ./farm.c ./fit.c ./image
+.c ./macro.c -L/opt/local/var/macports/software/gsl/1.12_0/opt/local/lib -o farm" >> test.sh
 
-while [ $i -le $np ]
-do
-    if [ $(echo $TAGNAME | grep MODULE ) ]; then
-	echo "mpirun -np $i ./farm $x $y >> farm_$TAGNAME &" >> test.sh
-	echo "./camera" >> test.sh
-    else	
-	echo "mpirun -np $i ./farm $x $y >> farm_$TAGNAME" >> test.sh
-    fi
-    i=$[$i+1]
+	n=1
+	#diverse dimensioni
+	while [ $n -le $num_dim ]
+	do
+		#leggo la dimensione da file
+		dim=$(head -n $n $FILE_DIM | tail -n 1)
+		#aggiorno il nome del file di output
+		TAGNAME="$FLAG_TAGNAME-$dim"
+		#echo $TAGNAME
+		#diversi gradi di parallelismo
+		i=$[$MIN_P]
+		while [ $i -le $np ]
+		do
+			if [ $(echo $TAGNAME | grep MODULE ) ]; then
+				echo "mpirun -np $i ./farm $dim $dim >> farm_$TAGNAME &" >> test.sh
+				echo "./camera" >> test.sh
+			else	
+				echo "mpirun -np $i ./farm $dim $dim >> farm_$TAGNAME" >> test.sh
+			fi
+			
+			i=$[$i+1]
+		done
+		
+		n=$[$n+1]	
+	done
+	
+	k=$[$k+1]
 done
